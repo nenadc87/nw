e0277c8ffc8d39a917319532f62eeb905fd22010
Nenad Cikojevic <a01549084@unet.univie.ac.at>
2020-07-29 21:48:40 +0200

    billa test update

--- nightwatch_with_cucumber_v2/step-definitions/billa.js
+++ nightwatch_with_cucumber_v2/step-definitions/billa.js
@@ -1,26 +1,24 @@
 const { client } = require('nightwatch-api');
-const { Given, Then, When } = require('cucumber');   
+const { Given, Then, When } = require('cucumber'); 
+const url = 'https://www.billa.at'; 
+const productEP ='/produkte/da-komm-ich-her-gurke/00-408966';
          
 Given('I open Billa Online Shop page', function () {
   
-  return client.url('https://www.billa.at/')
-                .maximizeWindow()
+  return client.url(url)
+               // .maximizeWindow()
                 .waitForElementVisible('body',1000);
 });
 
 Given('I\'m loged in on the Billa Page with usename {string} and password {string}', function (username, password) {
-  
-
-  let button = (client)=>{
-    let buttons =  client.elements('css selector', 'button.button--arrow[type=submit]');
-    for(let i=0;i<buttons.size;++i){
-        if(buttons[i].textContent=="Anmelden")
-          return buttons[i]; 
-    }
-  }
- // console.log(button);
-
-  return client
+//   let button = (client)=>{
+//     let buttons =  client.elements('css selector', 'button.button--arrow[type=submit]');
+//     for(let i=0;i<buttons.size;++i){
+//         if(buttons[i].textContent=="Anmelden")
+//           return buttons[i]; 
+//     }
+//   }
+ return client
                 .click('.header__user-buttons-item button[aria-label=Anmelden]')
                 .waitForElementVisible('div[data-dd-class-if-flapout=account]', 1000)
                 .setValue('input[name=Email]', username)
@@ -32,40 +30,44 @@ Given('I\'m loged in on the Billa Page with usename {string} and password {strin
 
 });
 
-
-
-When('I search for {string}', function (string) {
+When('I search for {string}', function (articleName) {
   
-  return 'pending';
+    return client
+            .setValue('input[name=searchTerm]',articleName)
+            .keys('\uE007')
+            .waitForElementVisible('h1.h2') //Suchergebnisse title
+            .getAttribute('input[name=searchTerm]','$$currentValue', function(result) {
+                client.assert.equal(articleName,result['value'])});
+
 });
 
-
-
-
-Then('the the product with name {string} should appear', function (string) {
+Then('the the product with name {string} should appear', function (productTitle) {
   
-  return 'pending';
+    return client
+            .waitForElementVisible('a[href="'+productEP+'"]')
+            //This doesn't work 
+            //.assert.containsText('a[href="/produkte/da-komm-ich-her-gurke/00-408966"]',productTitle) 
+            .getAttribute('a[href="'+productEP+'"]','textContent', function(result) {
+                client.assert.equal(productTitle,result['value'])});
+                //console.log('result',result['value'])});
 });
 
-
-
-
-When('I go to the detail view of the product with name {string}', function (string) {
+When('I go to the detail view of the product with name {string}', function (productTitle) {
   
-  return 'pending';
+  return client.click('a[href="'+productEP+'"]').waitForElementVisible('.product-detail__desc-title');
 });
 
-
-
-Then('I land to the detail view of the {string} product', function (string) {
-  
-  return 'pending';
+Then('I land to the detail view of the {string} product', function (productTitle) {
+  return client
+            //.assert.containsText('product-detail__desc-title',productTitle);
+            .getAttribute('.product-detail__desc-title','textContent', function(result) {
+                client.assert.equal(productTitle,result['value'])});
 });
 
 
 
 When('I add {string} items to cart', function (string) {
-  
+ 
   return 'pending';
 });
 
