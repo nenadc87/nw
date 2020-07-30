eb8dea35024c9492351e3444914fbf4a9f9f1c51
Nenad Cikojevic <n.cikojevic@emakina.at>
2020-07-30 16:26:22 +0200

    billa

--- nightwatch_with_cucumber_v2/features/billa.feature
+++ nightwatch_with_cucumber_v2/features/billa.feature
@@ -9,11 +9,11 @@ Scenario: Searching for cucumber
   When I go to the detail view of the product with name "Da komm ich her! Gurke aus Österreich"
   Then I land to the detail view of the "Da komm ich her! Gurke aus Österreich" product
   When I add "6" items to cart
-  Then the cart shows 6 Items 
+  Then the cart shows "6" Items 
   #hover to cart to check amount of items
   When I go to the Warenkorb
   Then "Da komm ich her! Gurke aus Österreich" is in it "6" times
-  When I go to Filialenfinder (go with button- upper right)
+  When I go to Filialenfinder -go with button- upper right
   Then I enter to a search bar "1150" 
   Then the pin with the hover- text "Mariahilferstrasse 186-188" shows up
 
--- nightwatch_with_cucumber_v2/step-definitions/billa.js
+++ nightwatch_with_cucumber_v2/step-definitions/billa.js
@@ -6,7 +6,7 @@ const productEP ='/produkte/da-komm-ich-her-gurke/00-408966';
 Given('I open Billa Online Shop page', function () {
   
   return client.url(url)
-               // .maximizeWindow()
+                .maximizeWindow()
                 .waitForElementVisible('body',1000);
 });
 
@@ -27,7 +27,6 @@ Given('I\'m loged in on the Billa Page with usename {string} and password {strin
                 //.click(button)
                 .waitForElementVisible('.header__account-text')
                 .assert.containsText('.header__account-text', 'Mein Konto')
-
 });
 
 When('I search for {string}', function (articleName) {
@@ -44,7 +43,7 @@ When('I search for {string}', function (articleName) {
 Then('the the product with name {string} should appear', function (productTitle) {
   
     return client
-            .waitForElementVisible('a[href="'+productEP+'"]')
+            .waitForElementVisible('a[href="'+productEP+'"]',15000)
             //This doesn't work 
             //.assert.containsText('a[href="/produkte/da-komm-ich-her-gurke/00-408966"]',productTitle) 
             .getAttribute('a[href="'+productEP+'"]','textContent', function(result) {
@@ -54,7 +53,8 @@ Then('the the product with name {string} should appear', function (productTitle)
 
 When('I go to the detail view of the product with name {string}', function (productTitle) {
   
-  return client.click('a[href="'+productEP+'"]').waitForElementVisible('.product-detail__desc-title');
+  //" Error while running .clickElement() protocol action: stale element reference: element is not attached to the page document" 
+  return client.click('css selector','a[href="'+productEP+'"]').waitForElementVisible('.product-detail__desc-title');
 });
 
 Then('I land to the detail view of the {string} product', function (productTitle) {
@@ -66,55 +66,73 @@ Then('I land to the detail view of the {string} product', function (productTitle
 
 
 
-When('I add {string} items to cart', function (string) {
- 
-  return 'pending';
-});
-
-
-
-
-Then('the cart shows {int} Items', function (int) {
-// Then('the cart shows {float} Items', function (float) {
+When('I add {string} items to cart', function (amount) {
   
-  return 'pending';
+  // return client.elements('css selector','input[aria-label=Menge]', function(result) {
+  //   for(let i=0;i<result.value.length;++i){
+  //       console.log(result.value[i].getAttribute('baseURI'));
+  //       if(result.value[i]['baseURI'].equal(url+productEP))
+  //   }
+  // });
+  return client
+            .waitForElementVisible('input[aria-label="Menge"]')
+            //.clearValue('input[aria-label="Menge"]')
+            .click('button.quantity__plus')
+            .click('button.quantity__plus')
+            .click('button.quantity__plus')
+            .click('button.quantity__plus')
+            .click('button.quantity__plus')
+            //.setValue('input[aria-label="Menge"]',amount)
+            .getAttribute('input[aria-label="Menge"]','value', function(result) {
+              console.log(result['value']);
+              client.assert.equal(amount,result['value'])})
+            .click('button[title="in den Warenkorb"]');
 });
 
-
-
-
+Then('the cart shows {string} Items', function (amount) {
+  return client
+            .moveToElement('.badge.badge--important',5,5)
+            .waitForElementVisible('[ng-bind="notifyNewBasketItem.count"]',15000)
+            //.assert.visible('.product-list__product__amount');
+            .assert.containsText('[ng-bind="notifyNewBasketItem.count"]',amount)
+});
 When('I go to the Warenkorb', function () {
+  return client.click('.header__cart-icon-wrapper.relative').waitForElementVisible('h1.h2');
+});     
+
+Then('{string} is in it {string} times', function (productTitle, amount) {
   
-  return 'pending';
+  return client
+           // .assert.containsText('.product-list__product__name',productTitle)
+            .getAttribute('.product-list__product__name','textContent', function(result) {
+            client.assert.equal(productTitle,result['value'])})
+            .getAttribute('input.quantity__input[type="text"]','value', function(result) {
+            client.assert.equal(amount,result['value'])});
 });
 
 
-       
-
-         Then('{string} is in it {string} times', function (string, string2) {
-           
-           return 'pending';
-         });
-
-
-       
-
-         When('I go to Filialenfinder \(go with button- upper right)', function () {
-           
-           return 'pending';
-         });
-
-       
-
-         Then('I enter to a search bar {string}', function (string) {
-           
-           return 'pending';
-         });
-
+When('I go to Filialenfinder -go with button- upper right', function () {
   
-       
+  return client
+          .click('a[title="Filialfinder"]')
+          .waitForElementVisible('div.flex__column.store-finder__column--top')
+          .assert.visible('div.flex__column.store-finder__column--top');
+});
 
-         Then('the pin with the hover- text {string} shows up', function (string) {
-           
-           return 'pending';
-        });
+Then('I enter to a search bar {string}', function (districtNumber) {
+  
+  return client
+          .setValue('#searchStore',districtNumber)
+          .pause(2000)
+          .keys(client.Keys.ENTER);
+});
+
+Then('the pin with the hover- text {string} shows up', function (streetName) {
+  
+  return client
+          .moveToElement('div[title="Mariahilferstrasse 186-188"]',2,2)
+          //??
+          .assert.attributeContains('div[title="Mariahilferstrasse 186-188"]','title', streetName);
+         
+
+});
